eb9d99387dd2e86658edd560daa9cf55a21f5324
Pranshu <pranshgupta54@gmail.com>
2023-09-07 04:19:26 +0530

    .

---
+++ fetchContests.js
@@ -0,0 +1,31 @@
+const https = require('https');
+require('dotenv').config();
+async function fetchContestsData() {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: process.env.BACKEND_URL,
+      path: '/api/contests',
+      method: 'GET',
+    };
+
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+        res.on('end', () => {
+        //   console.log("Req Sent to Server")
+        resolve(data);
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+}
+
+module.exports = fetchContestsData;
--- index.js
+++ index.js
@@ -2,21 +2,54 @@ const express = require('express');
 const axios = require('axios');
 const xmlbuilder = require('xmlbuilder');
 const { DateTime } = require('luxon'); // Luxon library for date and time manipulation
-
+const fetchContestsData = require('./fetchContests');
 const app = express();
+require('dotenv').config();
+
+let contestsData = null; // Variable to store fetched contest data
+let lastFetchTime = null; // Variable to store the time of the last fetch
 
 // Function to convert Unix timestamp to IST time
 function convertUnixToIST(unixTimestamp) {
   return DateTime.fromSeconds(unixTimestamp).toFormat('yyyy-MM-dd HH:mm:ss ZZZZ');
 }
 
+// Function to fetch contest data from the API
+async function fetchContests() {
+  try {
+    console.log('Fetching contest data...');
+    const response = await axios.get('https://digitomize-backend.onrender.com/api/contests'); // Your API URL
+    contestsData = response.data.results;
+    lastFetchTime = new Date();
+    console.log('Contest data fetched successfully.');
+  } catch (error) {
+    console.error('Error fetching contest data:', error);
+  }
+}
+
+// Initial fetch of contest data
+fetchContests();
+
+// Ping the server every 14 minutes and fetch contest data if more than 6 hours have passed
+setInterval(async () => {
+    try {
+        await fetchContests();
+        console.log('<======= Sent GET request to AWAKE');
+    } catch (error) {
+        console.error('Error Pinging', error);
+    }
+}, 14 * 60 * 1000);
+
 // Endpoint for generating and returning the sitemap XML
 app.get('/sitemap.xml', async (req, res) => {
   try {
-    // Make a GET request to your API to fetch contest data
-    const apiUrl = 'https://digitomize-backend.onrender.com/api/contests'; // Your API URL
-    const response = await axios.get(apiUrl);
-    const contests = response.data.results; // Assuming the contest data is in the 'results' property
+    // Use the contestsData if it exists and was fetched within the last 6 hours
+    if (contestsData && lastFetchTime && (new Date() - lastFetchTime) < 6 * 60 * 60 * 1000) {
+      console.log('Using cached contest data.');
+    } else {
+      // Fetch contest data if it's not available or if it's too old
+      await fetchContests();
+    }
 
     // Create the sitemap XML
     const root = xmlbuilder.create('urlset', {
@@ -25,7 +58,7 @@ app.get('/sitemap.xml', async (req, res) => {
     });
     root.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
-    contests.forEach((contest) => {
+    contestsData.forEach((contest) => {
       const contestUrl = `https://www.digitomize.com/contests/${contest.vanity}`;
       const title = `${contest.name} | Digitomize`;
       const startTimeIST = convertUnixToIST(contest.startTimeUnix);
@@ -52,7 +85,7 @@ app.get('/sitemap.xml', async (req, res) => {
     res.header('Content-Type', 'application/xml');
     res.send(sitemapXML);
   } catch (error) {
-    console.error('Error fetching contest data:', error);
+    console.error('Error fetching or generating sitemap:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
