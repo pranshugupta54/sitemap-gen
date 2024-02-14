2cbc71659f1b3c3cacd31cd3044a280e394aee29
Pranshu Gupta <pranshgupta54@gmail.com>
2024-02-14 16:40:08 +0530

    Update fetchUsers.js

--- fetchUsers.js
+++ fetchUsers.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/digitomize';
+const url = process.env.MONGO_URI || 'mongodb://localhost:27017/digitomize';
 
 let myUsers = [];
 
